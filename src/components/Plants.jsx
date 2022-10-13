896dc20ee3156975f2f66cf8f748bbff62722525
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2022-10-13 16:54:56 +0300

    imported Plant element

--- src/components/Plants.jsx
+++ src/components/Plants.jsx
@@ -1,5 +1,13 @@
 import React from "react"
+import Plant from "./Plant"
 
+/**
+ * This is the page where all the plants whose available to the public to buy
+ */
 export default function Plants() {
-	return <h1>Hello from Plants</h1>
+	return (
+		<main className="px-20 py-8">
+			<Plant />
+		</main>
+	)
 }
