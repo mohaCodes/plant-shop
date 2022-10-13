26feecd0f937bfa4a6de144bdae850e6048b00f2
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2022-10-13 16:31:02 +0300

    created and some initial info has been added

---
+++ src/plantsData.js
@@ -0,0 +1,11 @@
+
+// this array of objects will contain information that will be passed as arguments to Plant element
+export default [
+    {
+        id: 1,
+        title: "Snake Plant",
+        plantImage: "https://images.unsplash.com/photo-1507988914355-bf49fdbc7368?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=378&q=80",
+        price: 16
+
+    }
+]
