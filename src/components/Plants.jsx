c59bb7502a34498bef88e502ef8c0709b2a04bfc
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2022-10-14 01:29:16 +0300

    rendered Plant element with .map()

--- src/components/Plants.jsx
+++ src/components/Plants.jsx
@@ -1,13 +1,19 @@
 import React from "react"
 import Plant from "./Plant"
+import plantsData from "../plantsData"
 
 /**
  * This is the page where all the plants whose available to the public to buy
  */
 export default function Plants() {
+	// rendering plants with .map()
+	const renderPlantsCards = plantsData.map((item) => {
+		return <Plant item={item} key={item.id} />
+	})
+
 	return (
-		<main className="px-20 py-8">
-			<Plant />
+		<main className="px-20 py-8 flex flex-row gap-x-10">
+			{renderPlantsCards}
 		</main>
 	)
 }
