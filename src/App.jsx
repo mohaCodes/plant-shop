a4808da558601260549cbf20988f0ef4a52e2c12
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2022-10-14 22:45:08 +0300

    added a state to make items in cart dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,35 @@ import Home from "./components/Home"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
+	/**
+	 * This state is the number of items added in cart, It'll be passed to <Navbar/>.
+	 */
+	const [itemsInCart, setItemsInCart] = React.useState(0)
+
+	/**
+	 * This function will be passed to <Plants/> and then passed to <Plant/> as a onClick Event,
+	 * It calls setItemsInCart() and then updates the UI.
+	 */
+	function updateItemsInCart() {
+		setItemsInCart((prev) => {
+			return prev + 1
+		})
+	}
+
 	return (
 		<BrowserRouter>
 			<div>
-				<Navbar />
+				<Navbar itemsInCart={itemsInCart} />
 				<Routes>
 					<Route path="/" element={<Home />} />
-					<Route path="/plants" element={<Plants />} />
+					<Route
+						path="/plants"
+						element={
+							<Plants
+								onCLickPassedToPlant={updateItemsInCart}
+							/>
+						}
+					/>
 				</Routes>
 			</div>
 		</BrowserRouter>
